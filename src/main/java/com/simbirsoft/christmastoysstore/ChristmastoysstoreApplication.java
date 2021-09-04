79ad035d7bb89cbe9f010c29988ad73bec757610
Drakoniy <lubenskii_fedor@mail.ru>
2021-09-04 21:13:48 +0300

    Created!

---
+++ src/main/java/com/simbirsoft/christmastoysstore/ChristmastoysstoreApplication.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.christmastoysstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmastoysstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmastoysstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simbirsoft/christmastoysstore/ChristmastoysstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.christmastoysstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristmastoysstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
